d9aec3ecb7ac1f84fd44f312868cb85be7961f4c
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-05-28 10:57:50 -0700

    Changed database models to use uuid.  Need to change logic to use uuid as well

--- applications/ipos/models/3_company_description.py
+++ applications/ipos/models/3_company_description.py
@@ -1,7 +1,12 @@
 # Create 
 db.define_table('company_description',
+                    Field('uuid',length=64,default=lambda:str(uuid.uuid4())),
+                    Field('modified_on', 'datetime', default=request.now),
                     Field('description',type='string',notnull=True),
-                    Field('company_id','reference company_info',notnull=True),
-                    Field('data_source_id','reference data_source')
+                    Field('company_id',length=64,notnull=True),
+                    Field('data_source_id',length=64),
+                    format='%(description)s'
                    )
+
+db.company_description.company_id.requires=IS_IN_DB(db,'company_info.uuid','%(name)')
 
