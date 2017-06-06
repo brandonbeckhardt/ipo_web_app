bee7860a22ba8749b0afaeed94306942765204aa
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-06-06 16:07:09 -0700

    Allowed sqlite_local in .env to use sqlite local

--- gluon/contrib/heroku.py
+++ gluon/contrib/heroku.py
@@ -9,6 +9,9 @@ import os
 from gluon import *
 from pydal.adapters import ADAPTERS, PostgreSQLAdapter
 from pydal.helpers.classes import UseDatabaseStoredFile
+import logging
+logger = logging.getLogger("web2py.app.ipos")
+logger.setLevel(logging.DEBUG)
 
 class HerokuPostgresAdapter(UseDatabaseStoredFile,PostgreSQLAdapter):
     drivers = ('psycopg2',)
@@ -17,14 +20,21 @@ class HerokuPostgresAdapter(UseDatabaseStoredFile,PostgreSQLAdapter):
 ADAPTERS['postgres'] = HerokuPostgresAdapter
 
 def get_db(name = None, pool_size=10):
+    logger.info(os.environ[name])
     if not name:
         names = [n for n in os.environ.keys()
                  if n[:18]+n[-4:]=='HEROKU_POSTGRESQL__URL']
         if names:
             name = names[0]
-    if name:
+            logger.info("Using db " + name)
+    if name and os.environ[name] == 'sqlite_local':
+        db = DAL('sqlite://heroku.test.sqlite')
+        logger.info('sqlite_local set, using sqlite')
+    elif name:
         db = DAL(os.environ[name], pool_size=pool_size)
         current.session.connect(current.request, current.response, db=db)
+        logger.info('Using postgres db')
     else:
         db = DAL('sqlite://heroku.test.sqlite')
+        logger.info('Unable to find postgres db, using sqlite')
     return db
