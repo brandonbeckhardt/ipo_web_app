1f34b055e672e66ad3883786814498e7e46edcff
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-06-29 18:59:15 -0700

    Fixed the way we display urls

--- applications/ipos/modules/UrlHandler.py
+++ applications/ipos/modules/UrlHandler.py
@@ -29,25 +29,19 @@ class UrlHandler:
 				self.brokerUrls[row.reference_id] = row
 
 	def getCompanyUrl(self, group, match):
-	    url = ""
-	    if group.upper() == DateGroupNames.PAST: #in the past
-	        if match['company_id'] in self.publicCompanyUrls and self.publicCompanyUrls[match['company_id']].url != "":
-	        	url = self.publicCompanyUrls[match['company_id']].url
-	       	elif match['company_id'] in self.privateCompanyUrls:
-	       		url = self.privateCompanyUrls[match['company_id']].url
-	    else:
-	        if (match['ipo_date'] and DateHandling.dateToDatetime(match['ipo_date']) >= DateHandling.today() 
-	        and match['company_id'] in self.publicCompanyUrls and self.publicCompanyUrls[match['company_id']].url != ""):
-	           url = self.publicCompanyUrls[match['company_id']].url
-	        elif match['company_id'] in self.privateCompanyUrls: #use private if exists
-	        	url = self.privateCompanyUrls[match['company_id']].url
-	    if url != "":
-	    	if (url[:4] != 'http' and url[:5] != 'https'):
-	    		if (url[:4] == 'www.'):
-	    			url = "http://" + url
-	    		else:
-	    			url = "http://www." + url
-	    return url
+		url = ""
+		if match['company_id'] in self.publicCompanyUrls and self.publicCompanyUrls[match['company_id']].url != "":
+			url = self.publicCompanyUrls[match['company_id']].url
+		elif match['company_id'] in self.privateCompanyUrls:
+			url = self.privateCompanyUrls[match['company_id']].url
+
+		if url != "":
+			if (url[:4] != 'http' and url[:5] != 'https'):
+				if (url[:4] == 'www.'):
+					url = "http://" + url
+				else:
+					url = "http://www." + url
+		return url
 
 
 
