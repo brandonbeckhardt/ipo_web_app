c8e3c844ffc58a9aed2e6e90f8555fa36533e208
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-08-06 15:18:04 -0700

    Temp save to checkout

--- applications/ipos/static/js/ipo.js
+++ applications/ipos/static/js/ipo.js
@@ -1,28 +1,26 @@
 var showOrHideGroupTable = function(button, table) {
     if (table.style.display === 'none') {
         table.style.display = '';
-    	button.innerHTML = '-';
+        button.innerHTML = '-';
     } else {
         table.style.display = 'none';
-    	button.innerHTML = '+';
+        button.innerHTML = '+';
     }
 }
 
 var sortMatches = function(matchesWrapper, matches, group, edit, urlHandler) {
-	console.log(matches)
-	console.log(urlHandler)
-    console.log("--")
-    console.log(group)
-	$.ajax({url: "default/matcher_table", 
-		// Will want to change how we send data in the future
-		data: {'edit':edit, 'matches':JSON.stringify(matches),  
-		'group': JSON.stringify(group), 'urlHandler':urlHandler},
-		success: function(result){
-			console.log(result)
+    $.ajax({url: "default/matcher_table", 
+        // contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
+        // dataType: 'json',
+        // Will want to change how we send data in the future
+        data: {'edit':edit, 'matches':JSON.stringify(matches),  
+        'group': JSON.stringify(group), 'urlHandler':urlHandler},
+        success: function(result){
+            console.log(result)
             var groupIdentifier = group[1]
             var divIdRef = "#table_wrapper_" + groupIdentifier
             var tableWrapperRef = "#table_wrapper_group_" + groupIdentifier
             $( divIdRef ).html( $( result ).filter( tableWrapperRef ).html() );
-        console.log(result)
-    }});
+            console.log(result)
+        }});
 }
