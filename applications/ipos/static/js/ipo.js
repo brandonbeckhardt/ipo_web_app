75dcfeb67cb743cb159321db54f26af97e43f826
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-08-06 17:59:05 -0700

    Sorting works by ascending only.  Need to add descending, also handling of url data.  May want to add reset button

--- applications/ipos/static/js/ipo.js
+++ applications/ipos/static/js/ipo.js
@@ -8,16 +8,17 @@ var showOrHideGroupTable = function(button, table) {
     }
 }
 
-var sortMatches = function(matches, group, edit, urlHandler) {
+var sortMatches = function(sortBy, order, matches, group, edit, urlHandler) {
+    console.log(matches);
+    console.log(sortBy);
     $.ajax({url: "default/matcher_table", 
         data: {'edit':edit, 'matches':JSON.stringify(matches),  
-        'group': JSON.stringify(group), 'urlHandler':urlHandler},
+        'group': JSON.stringify(group), 'urlHandler':urlHandler,
+        'sortBy':sortBy, 'order':order},
         success: function(result){
-            console.log(result)
             var groupIdentifier = group[1]
             var divIdRef = "#table_wrapper_" + groupIdentifier
             var tableWrapperRef = "#table_wrapper_group_" + groupIdentifier
             $( divIdRef ).html( $( result ).filter( tableWrapperRef ).html() );
-            console.log(result)
         }});
 }
